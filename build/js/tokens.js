0b00e1dfbd97d093a451cb72b285fbced1ae025c
Adriely Lopes <adrielylopes@Adrielys-MacBook-Air.local>
2025-06-12 15:12:08 -0300

    first commit

---
+++ build/js/tokens.js
@@ -0,0 +1,14 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+:root {
+  --Color5050: #50505c;
+  --Color7070: #707072;
+  --Color27272: #27272a;
+  --ColorWhite: #d9d9d9;
+  --Color0909: #09090b;
+  --ColorPurplegray: #3a3545;
+  --VariableCollectionWhite: #ffffff;
+  --VariableCollectionStyles: #27272a;
+}
